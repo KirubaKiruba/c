07f6aa77bf902e90f7820ebbe4e7c35afec0b868
KirubaKiruba <kirubakaranvasanthi@gmail.com>
2025-07-21 17:58:59 +0530

    For loop

---
+++ for.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int i=7,j=7;
+for(i=0,j=6;j>=i;i++)          /* for(initialization(step 1); conditional check(step 2); control statement(Step 4))
+                                  statement1(Step 3);                     */
+printf("hi.....%d %d\n",i,j);
+
+}
